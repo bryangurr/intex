c95ded76ae6b95bc63c9050506bb62825fb16635
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-04-07 17:37:47 -0600

    Add HomePage with nav to login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { CartProvider } from "./context/CartContext";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-        <Router>
-          <Routes>
-
-            {/* <Route path="/cart" element={<CartPage />} /> */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-          </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route path="/Home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/cart" element={<CartPage />} /> */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
