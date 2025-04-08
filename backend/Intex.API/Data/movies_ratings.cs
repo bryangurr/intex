e22329f588b479366dc541dd8c83ab9c70c761f1
aezc2002 <aezc2002@outlook.com>
2025-04-08 09:46:20 -0600

    hello there

--- backend/Intex.API/Data/movies_ratings.cs
+++ backend/Intex.API/Data/movies_ratings.cs
@@ -7,6 +7,7 @@ namespace Intex.API.Data
     {
         [Key]
         public int user_id { get; set; }
+        //public movies_titles show_id { get; set; }
         public string? show_id { get; set; }
         public int? rating { get; set; }
     }
