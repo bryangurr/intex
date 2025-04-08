eb975d613323d030cccde8234c11a8f68b038586
aezc2002 <aezc2002@outlook.com>
2025-04-08 12:35:13 -0600

    apiiiiiiii

--- backend/Intex.API/Data/movies_titles.cs
+++ backend/Intex.API/Data/movies_titles.cs
@@ -14,7 +14,7 @@ namespace Intex.API.Data
         public string? country { get; set; }
         public int? release_year { get; set; }
         public string? rating { get; set; }
-        public int? duration { get; set; }
+        public string? duration { get; set; }
         public string? description { get; set; }
         public int? Action { get; set; }
         public int? Adventure { get; set; }
@@ -49,6 +49,6 @@ namespace Intex.API.Data
         public int? Talk_Shows_TV_Comedies { get; set; }
         public int? Thrillers { get; set; }
         public string? Genre { get; set; }
-        public float? Ratings_Avg { get; set; }
+        public decimal? Ratings_Avg { get; set; }
     }
 }
