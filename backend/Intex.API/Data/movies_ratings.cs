8bb7f3fb43ee9c8549b6766df13bbccc2ae3fa95
aezc2002 <aezc2002@outlook.com>
2025-04-07 21:29:37 -0600

    Classes, context, and controller (ish)

--- backend/Intex.API/Data/movies_ratings.cs
+++ backend/Intex.API/Data/movies_ratings.cs
@@ -2,5 +2,8 @@
 {
     public class movies_ratings
     {
+        public int? user_id { get; set; }
+        public string? show_id { get; set; }
+        public int? rating { get; set; }
     }
 }
