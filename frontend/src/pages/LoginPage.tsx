9090a3cb02062699dd8a5c18730868d0629cd144
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-04-07 21:20:24 -0600

    Correct the link to call the backend

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -40,8 +40,8 @@ function LoginPage() {
     }
 
     const loginUrl = rememberme
-      ? "https://yellow-stone-0c45d971e.6.azurestaticapps.net/login?useCookies=true"
-      : "https://yellow-stone-0c45d971e.6.azurestaticapps.net/login?useSessionCookies=true";
+      ? "https://cineniche-intex-cdadeqcjgwgygpgy.eastus-01.azurewebsites.net/login?useCookies=true"
+      : "https://cineniche-intex-cdadeqcjgwgygpgy.eastus-01.azurewebsites.net/login?useSessionCookies=true";
 
     try {
       const response = await fetch(loginUrl, {
