622c65dd1178acadd817a6fb1d3119b94ac0fd8b
Master-004 <153117410+Master-004@users.noreply.github.com>
2025-04-09 13:20:56 -0600

    Toby's updates (crossing fingers)

---
+++ backend/Intex.API/Data/RecommendationsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Intex.API.Data
+{
+    public class RecommendationsDbContext : DbContext
+    {
+        public RecommendationsDbContext(DbContextOptions<RecommendationsDbContext> options) : base(options)
+        {
+        }
+        public DbSet<recommendations> recommendations { get; set; }
+       
+    }
+}
