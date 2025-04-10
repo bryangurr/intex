e0ea7714ddeebd4590835a6254cb206f94621d94
aezc2002 <aezc2002@outlook.com>
2025-04-09 22:52:02 -0600

    CSP Header

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173, // or whatever you prefer
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' fonts.googleapis.com ; img-src 'self' data:; frame-ancestors 'none'; font-src 'self' fonts.gstatic.com data:; connect-src 'self' http://localhost:5000;",
+    },
   },
+  // makesure to change the https localhost:5000 to the production server url
 });
