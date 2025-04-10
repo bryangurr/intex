15324d1bf5094fd2d1ad9b4e2e109d99c9adc7eb
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-04-10 14:07:33 -0600

    Comment out unused React import

--- frontend/src/pages/AdminMoviePage.tsx
+++ frontend/src/pages/AdminMoviePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { useUser } from "../components/AuthorizeView";
 import { Navigate } from "react-router-dom";
 import AdminList from "../components/AdminList";
